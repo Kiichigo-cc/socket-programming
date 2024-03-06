1c0cb212665efbfea0f9d9e2ddca7724863dd4ee
Kiichigo-cc <oliveryzhou@gmail.com>
2024-03-05 19:09:54 -0800

    initial commit

---
+++ manager.py
@@ -0,0 +1,20 @@
+# Developed on Python 3.12.1
+# Install requirements with:
+# pip install -r requirements.txt
+"""
+
+"""
+
+__author__ = "Your Name"
+__version__ = "0.1.0"
+__license__ = "MIT"
+
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
